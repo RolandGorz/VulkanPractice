aa6f9932d6a22d9f7a1af157d921b2952ddbb00b
Roland Gorzkowski <gorzkowskiroland@gmail.com>
2024-06-16 00:32:13 -0400

    Initial vulkan implementation to validate support

--- src/main/java/my/game/HelloWorld.java
+++ src/main/java/my/game/HelloWorld.java
@@ -4,40 +4,18 @@ import org.lwjgl.Version;
 import org.lwjgl.glfw.Callbacks;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
-import org.lwjgl.glfw.GLFWVidMode;
-import org.lwjgl.opengl.GL;
-import org.lwjgl.opengl.GL46;
-import org.lwjgl.opengl.GL46C;
 import org.lwjgl.system.MemoryStack;
 import org.lwjgl.system.MemoryUtil;
+import org.lwjgl.vulkan.VK13;
 
+import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 
 public class HelloWorld {
 
-    // The window handle
-    private long window;
-
     public static void main(String[] args) {
-        new HelloWorld().run();
-    }
-
-    public void run() {
         System.out.println("Hello LWJGL " + Version.getVersion() + "!");
 
-        init();
-        loop();
-
-        // Free the window callbacks and destroy the window
-        Callbacks.glfwFreeCallbacks(window);
-        GLFW.glfwDestroyWindow(window);
-
-        // Terminate GLFW and free the error callback
-        GLFW.glfwTerminate();
-        GLFW.glfwSetErrorCallback(null).free();
-    }
-
-    private void init() {
         // Setup an error callback. The default implementation
         // will print the error message in System.err.
         GLFWErrorCallback.createPrint(System.err).set();
@@ -47,79 +25,36 @@ public class HelloWorld {
             throw new IllegalStateException("Unable to initialize GLFW");
 
         // Configure GLFW
-        GLFW.glfwDefaultWindowHints(); // optional, the current window hints are already the default
         GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE); // the window will stay hidden after creation
         GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_TRUE); // the window will be resizable
 
         // Create the window
-        window = GLFW.glfwCreateWindow(300, 300, "Hello World!", MemoryUtil.NULL, MemoryUtil.NULL);
+        final long window = GLFW.glfwCreateWindow(300, 300, "Hello World!", MemoryUtil.NULL, MemoryUtil.NULL);
         if (window == MemoryUtil.NULL)
             throw new RuntimeException("Failed to create the GLFW window");
 
-        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
-        GLFW.glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            if (key == GLFW.GLFW_KEY_ESCAPE && action == GLFW.GLFW_RELEASE)
-                GLFW.glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
-        });
-        GLFW.glfwSetMouseButtonCallback(window, (window, button, action, mods) -> {
-            if (action == GLFW.GLFW_PRESS) {
-                System.out.println("mouse pressed woohooo");
-                GL46.glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
-            } else {
-                System.out.println("mouse released woohooo");
-                GL46.glClearColor(0.9f, 0.5f, 0.5f, 0.0f);
-            }
-        });
 
-        // Get the thread stack and push a new frame
         try (MemoryStack stack = MemoryStack.stackPush()) {
-            IntBuffer pWidth = stack.mallocInt(1); // int*
-            IntBuffer pHeight = stack.mallocInt(1); // int*
-
-            // Get the window size passed to glfwCreateWindow
-            GLFW.glfwGetWindowSize(window, pWidth, pHeight);
-
-            // Get the resolution of the primary monitor
-            GLFWVidMode vidmode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
-
-            // Center the window
-            GLFW.glfwSetWindowPos(
-                    window,
-                    (vidmode.width() - pWidth.get(0)) / 2,
-                    (vidmode.height() - pHeight.get(0)) / 2
-            );
+            IntBuffer extensionCount = stack.mallocInt(1); // int*
+            int result  = VK13.vkEnumerateInstanceExtensionProperties((ByteBuffer) null, extensionCount, null);
+            System.out.printf("vkEnumerateInstanceExtensionProperties returned %d\n", result);
+            System.out.printf("%d extensions supported\n", extensionCount.get(0));
         } // the stack frame is popped automatically
 
-        // Make the OpenGL context current
         GLFW.glfwMakeContextCurrent(window);
         // Enable v-sync
         GLFW.glfwSwapInterval(1);
-
-        // Make the window visible
         GLFW.glfwShowWindow(window);
-    }
-
-    private void loop() {
-        // This line is critical for LWJGL's interoperation with GLFW's
-        // OpenGL context, or any context that is managed externally.
-        // LWJGL detects the context that is current in the current thread,
-        // creates the GLCapabilities instance and makes the OpenGL
-        // bindings available for use.
-        GL.createCapabilities();
-
-        // Set the clear color
-        GL46.glClearColor(0.5f, 0.5f, 0.0f, 0.0f);
-
-        // Run the rendering loop until the user has attempted to close
-        // the window or has pressed the ESCAPE key.
         while (!GLFW.glfwWindowShouldClose(window)) {
-            GL46C.glClear(GL46C.GL_COLOR_BUFFER_BIT | GL46C.GL_DEPTH_BUFFER_BIT); // clear the framebuffer
-
-            GLFW.glfwSwapBuffers(window); // swap the color buffers
-
-            // Poll for window events. The key callback above will only be
-            // invoked during this call.
             GLFW.glfwPollEvents();
         }
+
+        // Free the window callbacks and destroy the window
+        Callbacks.glfwFreeCallbacks(window);
+        GLFW.glfwDestroyWindow(window);
+
+        // Terminate GLFW and free the error callback
+        GLFW.glfwTerminate();
+        GLFW.glfwSetErrorCallback(null).free();
     }
 }
